314385ea2c890a10f36cc8175b59a75193beda9a
wadson joseph <jwadson666@gmail.com>
2021-12-17 11:26:41 -0500

    ajout du library leaflet et initialisation du map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit,OnChanges } from '@angular/core';
+import  * as L  from 'leaflet'
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
-
+export class MapComponent implements OnInit,AfterViewInit,OnChanges {
+  map!:L.Map;
   constructor() { }
+
 
   ngOnInit(): void {
   }
+ 
+  ngAfterViewInit(){
+    this.addMap()
+  }
 
+  ngOnChanges(){
+    console.log("change")
+  }
+
+  //map initialisation 
+  addMap(){
+    this.map = L.map('map',{
+      center:[18.509640864733182,-72.2307899249769],
+      zoom:12
+    })
+    this.addLayer()
+  }
+
+  //map layer initialisation 
+  addLayer(){
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      minZoom:12,
+      maxZoom:20,
+      attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }
+    ).addTo(this.map)
+  }
 }
